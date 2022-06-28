d548f3ed27123b38b7dc749df5f251022a45e611
Minyeong Park <79310601+minyeon9@users.noreply.github.com>
2022-06-28 23:38:24 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int year = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		year = sc.nextInt();
+		
+		if ( (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)) ) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+		
+	}
+
+}
