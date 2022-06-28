456f68c4e3e5b2955a7a7bd41979f56d2926bfa2
Minyeong Park <79310601+minyeon9@users.noreply.github.com>
2022-06-28 23:44:11 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= N; i++) {
+			for(int b = 1; b <= (N - i); b++) {
+				bw.write(" ");
+			}
+			for(int j = 1; j <= i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
