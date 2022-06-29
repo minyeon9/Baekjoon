90f28d9bfcc9c034f7212dd4bcfbfa343f10bff7
Minyeong Park <79310601+minyeon9@users.noreply.github.com>
2022-06-29 22:36:37 +0900

    Update ？？！.java

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-        String str = sc.nextLine();
+		String str = sc.nextLine();
 
-        System.out.println(str + "??!");
+		System.out.println(str + "??!");
+		
+		// while문으로 감싸면 런타임 에러를 뱉네..
 	}
 }
